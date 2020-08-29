df35203b223d27738e3c4fc14e6ec5882b1876f9
arpithagit <arpithagit@gmail.com>
2020-08-29 13:08:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-drzmyd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-drzmyd)
---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+    $(document).ready(function(){
+        $('.studentProfileInfoGroups').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots:true,                        
+            arrows: false,
+            autoplay: true, 
+            speed:1000
+        })
+    });
+  </script>   
